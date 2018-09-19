498b097d70e25088af40ea8c1f2bdc86ab0e3975
Mr_yinwei <mayinwei@hotmail.com>
2018-09-19 13:59:14 +0800

    tag 10

--- CustomAlert/CustomAlert.m
+++ CustomAlert/CustomAlert.m
@@ -769,32 +769,37 @@ typedef NS_ENUM(NSInteger, AlertType) {
 }
 
 -(NSBundle *)bundleWithBundleName:(NSString *)bundleName podName:(NSString *)podName{
-    if (bundleName == nil && podName == nil) {
-        @throw @"bundleName和podName不能同时为空";
-    }else if (bundleName == nil ) {
-        bundleName = podName;
-    }else if (podName == nil) {
-        podName = bundleName;
-    }
+//    if (bundleName == nil && podName == nil) {
+//        @throw @"bundleName和podName不能同时为空";
+//    }else if (bundleName == nil ) {
+//        bundleName = podName;
+//    }else if (podName == nil) {
+//        podName = bundleName;
+//    }
+//
+//
+//    if ([bundleName containsString:@".bundle"]) {
+//        bundleName = [bundleName componentsSeparatedByString:@".bundle"].firstObject;
+//    }
+//    //没使用framwork的情况下
+//    NSURL *associateBundleURL = [[NSBundle mainBundle] URLForResource:bundleName withExtension:@"bundle"];
+//    //使用framework形式
+//    if (!associateBundleURL) {
+//        associateBundleURL = [[NSBundle mainBundle] URLForResource:@"Frameworks" withExtension:nil];
+//        associateBundleURL = [associateBundleURL URLByAppendingPathComponent:podName];
+//        associateBundleURL = [associateBundleURL URLByAppendingPathExtension:@"framework"];
+//        NSBundle *associateBunle = [NSBundle bundleWithURL:associateBundleURL];
+//        associateBundleURL = [associateBunle URLForResource:bundleName withExtension:@"bundle"];
+//    }
+//
+//    NSAssert(associateBundleURL, @"取不到关联bundle");
+//    //生产环境直接返回空
+//    return associateBundleURL?[NSBundle bundleWithURL:associateBundleURL]:nil;
     
-    
-    if ([bundleName containsString:@".bundle"]) {
-        bundleName = [bundleName componentsSeparatedByString:@".bundle"].firstObject;
-    }
-    //没使用framwork的情况下
-    NSURL *associateBundleURL = [[NSBundle mainBundle] URLForResource:bundleName withExtension:@"bundle"];
-    //使用framework形式
-    if (!associateBundleURL) {
-        associateBundleURL = [[NSBundle mainBundle] URLForResource:@"Frameworks" withExtension:nil];
-        associateBundleURL = [associateBundleURL URLByAppendingPathComponent:podName];
-        associateBundleURL = [associateBundleURL URLByAppendingPathExtension:@"framework"];
-        NSBundle *associateBunle = [NSBundle bundleWithURL:associateBundleURL];
-        associateBundleURL = [associateBunle URLForResource:bundleName withExtension:@"bundle"];
-    }
-    
-    NSAssert(associateBundleURL, @"取不到关联bundle");
-    //生产环境直接返回空
-    return associateBundleURL?[NSBundle bundleWithURL:associateBundleURL]:nil;
+    NSBundle *bundleForClass = [NSBundle bundleForClass:[self class]];
+    NSString *stringsBundlePath = [bundleForClass pathForResource:bundleName ofType:@"bundle"];
+    bundle = [NSBundle bundleWithPath:stringsBundlePath] ?: bundleForClass;
+    return bundle;
 }
 
 //loading无文字
