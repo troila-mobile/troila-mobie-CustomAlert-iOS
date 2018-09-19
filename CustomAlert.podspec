418216fc589097999b6a42230cdcb69a58152c20
Mr_yinwei <mayinwei@hotmail.com>
2018-09-19 09:41:26 +0800

    pod 文件

---
+++ CustomAlert.podspec
@@ -0,0 +1,29 @@
+
+Pod::Spec.new do |s|
+  s.name             = 'CustomAlert'
+  s.version          = '0.0.1'
+  s.summary          = ' CustomAlert is very good'
+
+
+  s.description      = <<-DESC
+TODO: Add long description of the pod here.
+                       DESC
+
+  s.homepage         = 'https://github.com/troila-mobile/troila-mobie-CustomAlert-iOS'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'mayinwei' => '843912524@qq.com' }
+  s.source           = { :git => 'https://github.com/troila-mobile/troila-mobie-CustomAlert-iOS.git', :tag => 0.0.1 }
+  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+
+  s.ios.deployment_target = '8.0'
+
+  s.source_files = 'CustomAlert/**/*'
+  
+   s.resource_bundles = {
+     'CustomAlert' => ['CustomAlert/CustomAlertImage.bundle/*']
+   }
+
+   s.public_header_files = 'CustomAlert/**/*.h'
+   s.frameworks = 'UIKit', 'MapKit'
+end
---
+++ CustomAlert/CustomAlert.h
@@ -0,0 +1,100 @@
+//
+//  CustomAlert.h
+//  AlertTroila
+//
+//  Created by Admin on 2018/9/13.
+//  Copyright © 2018年 马银伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, CustomAlertStyle) {
+    CustomAlertStyleSuccess,//默认成功图片
+    CustomAlertStyleError,//默认错误图片
+    CustomAlertStyleWarning,//默认警告图片
+    CustomAlertStyleNone  //没有图片
+};
+
+typedef void (^complete)(NSInteger index,NSString *title);//回调block
+
+
+@interface CustomAlert : UIView
+
+/*设置样式*/
+
+
++(void)setBackgroundColor:(UIColor *)color;//设置背景色
+
++(void)setFontColor:(UIColor *)color;//设置简单框样式文字样色和对话框内容颜色,loading 文字
+
++(void)setFont:(UIFont *)font;//设置简单框样式文字样色和对话框内容字号,loading 文字
+
+
++(void)setTitleColor:(UIColor *)color;//设置对话框标题颜色  默认为#0C71FF
+
++(void)setTitleFont:(UIFont *)font;//设置对话框标题字号  默认为18
+
++(void)setButtonColor:(UIColor *)color;//设置对话框底部按钮颜色
+
++(void)setButtonFont:(UIFont *)font;//设置对话框底部按钮字号大小
+
++(void)setButtonFont:(UIFont *)font color:(UIColor *)color;//设置对话框底部按钮颜色和字号大小
+
++(void)setButtonColor:(UIColor *)color index:(NSInteger)index;//设置对话框底部按钮颜色，index:按钮位置
+
++(void)setButtonFont:(UIFont *)font index:(NSInteger)index;//设置对话框底部按钮字号大小，index:按钮位置
+
++(void)setButtonFont:(UIFont *)font color:(UIColor *)color index:(NSInteger)index;//设置对话框底部按钮颜色和字号大小 index:按钮位置
+
++(void)setAlertCornerRadius:(CGFloat)value;//设置显示框的圆角  默认为10
+
+/*初始化方法*/
+
+//显示成功提示
++(void)showSuccessWithMessage:(NSString *)message;
+
+//显示错误提示
++(void)showErrorWithMessage:(NSString *)message;
+
+//带遮罩层成功提示
++(void)showShadeSuccessWithMessage:(NSString *)message;
+
+//带遮罩层失败提示
++(void)showShadeErrorWithMessage:(NSString *)message;
+
+//自定义图片，简单按钮
++(void)showMessage:(NSString *)message image:(UIImage *)image;
+
+//带遮罩层成功提示
++(void)showShadeWithMessage:(NSString *)message image:(UIImage *)image;
+
+
+
+
+//自定义按钮，默认图片样式
++(void)showAlertWithButtonTitleArray:(NSArray<NSString *> *)titleArray style:(CustomAlertStyle)style title:(NSString *)title content:(NSString *)content complete:(complete)completeBlock;
+
+
+//自定义按钮，自定义图片
++(void)showAlertWithButtonTitleArray:(NSArray<NSString *> *)titleArray image:(UIImage *)image title:(NSString *)title content:(NSString *)content complete:(complete)completeBlock;
+
+
+//自带确定和取消，默认图片样式
++(void)showAlertFullWithStyle:(CustomAlertStyle)style title:(NSString *)title content:(NSString *)content complete:(complete)completeBlock;
+
+
+//自带确定和取消，自定义图片样式
++(void)showAlertFullWithImage:(UIImage *)image title:(NSString *)title content:(NSString *)content complete:(complete)completeBlock;
+
+//自带确定按钮,
++(void)showAlertFinishWithStyle:(CustomAlertStyle)style title:(NSString *)title content:(NSString *)content complete:(complete)completeBlock;
+
+//确定按钮，自定义图片样式
++(void)showAlertFinishWithImage:(UIImage *)image title:(NSString *)title content:(NSString *)content complete:(complete)completeBlock;
+
+//加载等待
++(void)showLoading;
+
++(void)showLoadingWithMessage:(NSString *)message;
++(void)dissmis;//隐藏
+@end
